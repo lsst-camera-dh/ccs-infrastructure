404cf7c2d30a218864874cff9ff87ff06179b4f2
Glenn Morris <gmorris@slac.stanford.edu>
2019-04-12 12:36:15 -0700

    SetupCCS: add ssh key stuff

--- SetUpCCS.sh
+++ SetUpCCS.sh
@@ -300,3 +300,27 @@ grubfile=/boot/efi/EFI/centos/grub.cfg
 [ -e $grubfile ] || grubfile=/boot/grub2/grub.cfg
 
 grub2-mkconfig -o $grubfile
+
+
+## ssh
+[ -d /root/.ssh ] || mkdir -m 700 /root/.ssh
+
+## chef creates this as a symlink to .public/authorized_keys,
+## presumably for afs.
+f=/root/.ssh/authorized_keys
+
+[ -e $f ] || ( umask 077 && touch $f )
+
+## This allows logins from hosts with the associated private key.
+grep -q "root@lsst-ss01" $f || cat >> $f <<EOF
+#
+# GPFS cluster name: lsst-ss01.slac.stanford.edu
+from="*.slac.stanford.edu" ssh-dss AAAAB3NzaC1kc3MAAACBAJVRTAdjoR/1Sir4/caVnv5uIIYpzJvZn8U2yUWa15mNhJlKNH+x0ZBCr5YtqHCkYDWq1lk42eLUgoQn0rhJTbp4AvOO6FCrP61cMyYgJgpfv56InBvhF7aWFwhJsPAym4cQC1/7znmQfR8iM6dxA8z2yThwpUdRAXT4s4c16y0bAAAAFQDuy9XdUIOYTf0Cx4+cP5tZuTWyzQAAAIBjkkDeIAI44VdwTFzubnj5oLU9oXYahibPkTHKyFGVfp330s5+AnOFITXFULPiqCzM0/QiVqZjbWdwDClMIW4OzVbAs4zZ38bpbA08FfCXQ9t9Q2jp6sdI0iDX+ZgBkU1KuDl8uFYV+P0WPiG6nQ90+uo9FRtEuCZNehnKPMJFqgAAAIAoRslF1H+MLA471jndzHIkIGPA8bqKsGSgjSEFEsR1yTnqyVQf2PwrjtIv2PrARNaP76ekeYcYF4+Ql+88hcvfFMUejc4IUTJDQ7U8XL08CzkiG2hZfR5jXlxNoSHpISUE1eEBhYeks4HJV8JjjMyap5ccUoh40N9ezePKdrSjSQ== root@lsst-ss01.slac.stanford.edu
+
+EOF
+
+## FIXME this won't work because until we get this key we cannot login
+## to other hosts. Need a common file-system (eg nfs).
+rsync -a lsst-mcm:.ssh/id_dsa .ssh/ || \
+    echo "Failed to copy .ssh/id_dsa - push from another host"
+
