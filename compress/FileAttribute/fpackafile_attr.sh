c1bc94a55a6d0bcc919ca7b67a350ee89fc95fc2
Glenn Morris <gmorris@slac.stanford.edu>
2019-10-02 10:43:07 -0700

    fits compression: formatting tweaks

--- compress/FileAttribute/fpackafile_attr.sh
+++ compress/FileAttribute/fpackafile_attr.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
-
 #
+# Compress $1 using fpack and reset mtime, atime.
 # Original script from Yousuke, LSSTTD-1335.
-#
-
-# process $1 with filename per line using fpack and reset mtime, atime
-
-#---------------------
 
 FPACK=/gpfs/slac/lsst/fs2/u1/devel/marshall/cfitsio/bin/fpack
 FOPTS="-g2"
 
 fitsfile=$1
-#-compress the file
 
-$FPACK $FOPTS $fitsfile
-rt=$?
-if [[ $rt -ne 0 ]] ; then
-        echo "fpack error: returned $rt"
-        exit $rt
-fi
+## Compress the file.
+$FPACK $FOPTS $fitsfile || {
+    rt=$?
+    echo "fpack error: returned $rt"
+    exit $rt
+}
 
 ## Reset timestamp and ownership to match the original file.
 touch -r ${fitsfile} ${fitsfile}.fz
-chown --reference=${fitsfile} ${fitsfile}.fz
+chown --reference=$fitsfile ${fitsfile}.fz
 
 ## Rename new file over the original.
 ls -lh --full-time $fitsfile ${fitsfile}.fz
--- compress/TimeBased/fpackafile.sh
+++ compress/TimeBased/fpackafile.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #
-# process $1 with filename per line using fpack and reset mtime, atime
-#---------------------
+# Compress $1 using fpack and reset mtime, atime.
+
 FPACK=/gpfs/slac/lsst/fs2/u1/devel/marshall/cfitsio/bin/fpack
 FOPTS="-g2"
 
 fitsfile=$1
-#-compress the file
-$FPACK $FOPTS $fitsfile
-rt=$?
-if [[ $rt -ne 0 ]] ; then
-        echo "fpack error: returned $rt"
-        exit $rt
-fi
+
+## Compress the file.
+$FPACK $FOPTS $fitsfile || {
+    rt=$?
+    echo "fpack error: returned $rt"
+    exit $rt
+}
 
 ## Reset timestamp and ownership to match the original file.
 touch -r ${fitsfile} ${fitsfile}.fz
