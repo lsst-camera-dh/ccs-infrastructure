c2c7a0dc6e5ac1e11129c7f50be456f1df00a202
Glenn Morris <gmorris@slac.stanford.edu>
2019-10-02 10:47:46 -0700

    fits compression: remove no attr version, replace with symlink

--- compress/FileAttribute/fpackafile_attr.sh
+++ compress/FileAttribute/fpackafile_attr.sh
@@ -23,6 +23,7 @@ chown --reference=$fitsfile ${fitsfile}.fz
 ls -lh --full-time $fitsfile ${fitsfile}.fz
 mv -v ${fitsfile}.fz ${fitsfile}
 
+## The time-based version did not have this attr step.
 ## Set file attribute.
 ## NB: not visible over NFS; not preserved by default tar, rsync, etc.
 setfattr -n user.fpack ${fitsfile}
