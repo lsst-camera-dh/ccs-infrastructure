83129db3a6e95054a0f47459947e3bbbc4bffe1f
Glenn Morris <gmorris@slac.stanford.edu>
2020-03-26 15:28:19 -0700

    SetUpCCS: use template for ssh key

--- SetUpCCS.sh
+++ SetUpCCS.sh
@@ -620,12 +620,7 @@ grep -q ^HISTSIZE /root/.bashrc || \
     [ -e $f ] || ( umask 077 && touch $f )
 
     ## This allows logins from hosts with the associated private key.
-    grep -q "root@lsst-ss01" $f || cat >> $f <<EOF
-#
-# GPFS cluster name: lsst-ss01.slac.stanford.edu
-from="*.slac.stanford.edu" ssh-dss AAAAB3NzaC1kc3MAAACBAJVRTAdjoR/1Sir4/caVnv5uIIYpzJvZn8U2yUWa15mNhJlKNH+x0ZBCr5YtqHCkYDWq1lk42eLUgoQn0rhJTbp4AvOO6FCrP61cMyYgJgpfv56InBvhF7aWFwhJsPAym4cQC1/7znmQfR8iM6dxA8z2yThwpUdRAXT4s4c16y0bAAAAFQDuy9XdUIOYTf0Cx4+cP5tZuTWyzQAAAIBjkkDeIAI44VdwTFzubnj5oLU9oXYahibPkTHKyFGVfp330s5+AnOFITXFULPiqCzM0/QiVqZjbWdwDClMIW4OzVbAs4zZ38bpbA08FfCXQ9t9Q2jp6sdI0iDX+ZgBkU1KuDl8uFYV+P0WPiG6nQ90+uo9FRtEuCZNehnKPMJFqgAAAIAoRslF1H+MLA471jndzHIkIGPA8bqKsGSgjSEFEsR1yTnqyVQf2PwrjtIv2PrARNaP76ekeYcYF4+Ql+88hcvfFMUejc4IUTJDQ7U8XL08CzkiG2hZfR5jXlxNoSHpISUE1eEBhYeks4HJV8JjjMyap5ccUoh40N9ezePKdrSjSQ== root@lsst-ss01.slac.stanford.edu
-
-EOF
+    grep -q "root@lsst-ss01" $f || cat ./ssh/${f##*/} >> $f
 
     ## FIXME this won't work because until we get this key we cannot login
     ## to other hosts. Need a common file-system (eg nfs).
