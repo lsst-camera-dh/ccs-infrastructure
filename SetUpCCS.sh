70e891db9fea2f353511b7a390f336cbaa64c544
Glenn Morris <gmorris@slac.stanford.edu>
2023-12-13 17:14:54 -0800

    SetUpCCS: enable crb repo on rhel9+

--- SetUpCCS.sh
+++ SetUpCCS.sh
@@ -50,6 +50,8 @@ fi
 pkgarchive=/lnfs/lsst/pkgarchive    # /gpfs/slac/lsst/fs2/u1/pkgarchive
 [ $my_system = slac ] || pkgarchive=/root
 
+[ $release -ge 9 ] && yum-config-manager --enable crb
+
 
 case $my_system in
     slac)
